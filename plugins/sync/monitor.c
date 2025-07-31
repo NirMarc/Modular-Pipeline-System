a5cfd348ffb1b06034c4f08791b5d7feeb2d492a
NirMarc <marciano.nir@gmail.com>
2025-07-31 18:37:06 +0300

    Implemented monitor.c

--- plugins/sync/monitor.c
+++ plugins/sync/monitor.c
@@ -2,21 +2,39 @@
 #include "monitor.h"
 
 int monitor_init(monitor_t* monitor) {
+    monitor->signaled = 0;
+    if (pthread_mutex_init(&monitor->mutex, NULL)) return -1;
+    if (pthread_cond_init(&monitor->cond, NULL)) return -1;
     return 0;
 }
 
 void monitor_destroy(monitor_t* monitor) {
+    pthread_mutex_destroy(&monitor->mutex);
+    pthread_cond_destroy(&monitor->cond);
     return;
 }
 
 void monitor_signal(monitor_t* monitor) {
+    pthread_mutex_lock(&monitor->mutex);
+    monitor->signaled = 1;
+    pthread_cond_signal(&monitor->cond);
+    pthread_mutex_unlock(&monitor->mutex);
     return;
 }
 
 void monitor_reset(monitor_t* monitor) {
+    pthread_mutex_lock(&monitor->mutex);
+    monitor->signaled = 0;
+    pthread_mutex_unlock(&monitor->mutex);
     return;
 }
 
 int monitor_wait(monitor_t* monitor) {
+    pthread_mutex_lock(&monitor->mutex);
+    while (!monitor->signaled) {
+        pthread_cond_wait(&monitor->cond, &monitor->mutex);
+    }
+    monitor->signaled = 0;
+    pthread_mutex_unlock(&monitor->mutex);
     return 0;
 }
