4052186181051b9a1a431da4d4f49e8bc10b33b6
NirMarc <marciano.nir@gmail.com>
2025-08-01 13:10:42 +0300

    Updated plugins

--- plugins/logger.c
+++ plugins/logger.c
@@ -18,3 +18,19 @@ const char* plugin_init(int queue_size) {
 const char* get_plugin_name(void) {
     return "logger";
 }
+
+const char* plugin_fini(void) {
+    return plugin_fini();
+}
+
+const char* plugin_place_work(const char* str) {
+    return plugin_place_work(str);
+}
+
+void plugin_attach(const char* (*next_place_work)(const char*)) {
+    return plugin_attach(next_place_work);
+}
+
+const char* plugin_wait_finished(void) {
+    return plugin_wait_finished();
+}
